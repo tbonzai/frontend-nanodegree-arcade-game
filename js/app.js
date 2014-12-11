ca90aa6e2a54cd91fe6bb19a464b8545ff1fd4bb
Tom Benzaia <tbenzaia@gmail.com>
2014-12-10 20:59:49 -0600

    Refectored much of the code to make it cleaner.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,115 @@
+/**
+  * This object contains details about the images in the game. It allows images
+  * with varying attributes to be swapped out for one another.
+*/
+var appImages = {
+	'bug': {
+		sprite: 'images/enemy-bug.png'
+		, startX: 200
+		, startY: 405
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 27
+		, characterOffsetBottom: 92
+		, characterOffsetLeft: 1
+		, characterOffsetRight: 99
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	},
+	'boy': {
+		sprite: 'images/char-boy.png'
+		, startX: 200
+		, startY: 405
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 13
+		, characterOffsetBottom: 88
+		, characterOffsetLeft: 16
+		, characterOffsetRight: 83
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	},
+	'cat-girl': {
+		sprite: 'images/char-cat-girl.png'
+		, startX: 200
+		, startY: 406
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 11
+		, characterOffsetBottom: 88
+		, characterOffsetLeft: 16
+		, characterOffsetRight: 83
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	},
+	'horn-girl': {
+		sprite: 'images/char-horn-girl.png'
+		, startX: 200
+		, startY: 406
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 11
+		, characterOffsetBottom: 88
+		, characterOffsetLeft: 6
+		, characterOffsetRight: 83
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	},
+	'pink-girl': {
+		sprite: 'images/char-pink-girl.png'
+		, startX: 200
+		, startY: 404
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 12
+		, characterOffsetBottom: 88
+		, characterOffsetLeft: 12
+		, characterOffsetRight: 88
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	},
+	'princess-girl': {
+		sprite: 'images/char-princess-girl.png'
+		, startX: 200
+		, startY: 413
+		, minX: 0
+		, maxX: 400
+		, minY: 0
+		, maxY: 400
+		, characterOffsetTop: 2
+		, characterOffsetBottom: 88
+		, characterOffsetLeft: 13
+		, characterOffsetRight: 87
+		, offScreenLeft: -271
+		, offScreenRight: 600
+		, imageHeight: 171
+		, imageWidth: 101
+	}
+};
+
+/**
+  * This is a super class from which all game characters will inherit methods and properties.
+*/
 var Character = function() {
 	this.x = 0;
 	this.y = 0;
@@ -7,42 +119,43 @@ var Character = function() {
 	this.maxX = 0;
 	this.minY = 0;
 	this.maxY = 0;
-	this.edgeOffsetLeft = 0;
-	this.edgeOffsetRight = 0;
-	this.edgeOffsetTop = 0;
-	this.edgeOffsetBottom = 0;
+	this.characterOffsetLeft = 0;
+	this.characterOffsetRight = 0;
+	this.characterOffsetTop = 0;
+	this.characterOffsetBottom = 0;
 	this.offScreenLeft = 0;
 	this.offScreenRight = 0;
+	this.imageHeight = 0;
+	this.imageWidth = 0;
 };
 
-Character.prototype.leftEdge = function() {
-	return this.x + this.edgeOffsetLeft;
+Character.prototype.getCharacterLeftEdge = function() {
+	return this.x + this.characterOffsetLeft;
 };
 
-Character.prototype.rightEdge = function() {
-	return this.x + this.edgeOffsetRight;
+Character.prototype.getCharacterRightEdge = function() {
+	return this.x + this.characterOffsetRight;
 };
 
-Character.prototype.topEdge = function() {
-	return this.y + this.edgeOffsetTop;
+Character.prototype.getCharacterTopEdge = function() {
+	return this.y + this.characterOffsetTop;
 };
 
-Character.prototype.bottomEdge = function() {
-	return this.y + this.edgeOffsetBottom;
+Character.prototype.getCharacterBottomEdge = function() {
+	return this.y + this.characterOffsetBottom;
 };
 
 // Enemies our player must avoid
 var Enemy = function(character, speed) {
-	this.sprite = playerImages[character].sprite;
-	this.edgeOffsetTop = playerImages[character].edgeOffsetTop;
-	this.edgeOffsetBottom = playerImages[character].edgeOffsetBottom;
-	this.edgeOffsetLeft = playerImages[character].edgeOffsetLeft;
-	this.edgeOffsetRight = playerImages[character].edgeOffsetRight;
-	this.offScreenLeft = playerImages[character].offScreenLeft;
-	this.offScreenRight = playerImages[character].offScreenRight;
+	this.sprite = appImages[character].sprite;
+	this.characterOffsetTop = appImages[character].characterOffsetTop;
+	this.characterOffsetBottom = appImages[character].characterOffsetBottom;
+	this.characterOffsetLeft = appImages[character].characterOffsetLeft;
+	this.characterOffsetRight = appImages[character].characterOffsetRight;
+	this.offScreenLeft = appImages[character].offScreenLeft;
+	this.offScreenRight = appImages[character].offScreenRight;
 	this.speed = speed;
-	this.currentSpeed = speed;
-	this.flyOffSpeed = 0;
+	this.stopped = false;
 	this.getRandomStart();
 };
 
@@ -55,10 +168,12 @@ Enemy.prototype.update = function(dt) {
 	// which will ensure the game runs at the same speed for
 	// all computers.
 	// Check the x coordinate to see if they are on the screen.
-	if (this.x < this.offScreenRight) {
-		this.x += dt * this.currentSpeed;
-	} else {
-		this.getRandomStart();
+	if (!this.stopped) {
+		if (this.x < this.offScreenRight) {
+			this.x += dt * this.speed;
+		} else {
+			this.getRandomStart();
+		}
 	}
 };
 
@@ -69,7 +184,7 @@ Enemy.prototype.render = function() {
 
 Enemy.prototype.getRandomStart = function() {
 	// The lanes start at y coordinate 62 and have an 83px spacing.
-	this.y = (Math.floor((Math.random() * 3)) * 83) + (this.edgeOffsetBottom - this.edgeOffsetTop - 3);
+	this.y = (Math.floor((Math.random() * 3)) * 83) + (this.characterOffsetBottom - this.characterOffsetTop - 3);
 	this.x = this.offScreenLeft;
 };
 
@@ -79,124 +194,28 @@ Enemy.prototype.getRandomStart = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var playerImages = {
-	'bug': {
-		sprite: 'images/enemy-bug.png'
-		, startX: 200
-		, startY: 405
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 27
-		, edgeOffsetBottom: 92
-		, edgeOffsetLeft: 1
-		, edgeOffsetRight: 99
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	},
-	'boy': {
-		sprite: 'images/char-boy.png'
-		, startX: 200
-		, startY: 405
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 13
-		, edgeOffsetBottom: 88
-		, edgeOffsetLeft: 16
-		, edgeOffsetRight: 83
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	},
-	'cat-girl': {
-		sprite: 'images/char-cat-girl.png'
-		, startX: 200
-		, startY: 406
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 11
-		, edgeOffsetBottom: 88
-		, edgeOffsetLeft: 16
-		, edgeOffsetRight: 83
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	},
-	'horn-girl': {
-		sprite: 'images/char-horn-girl.png'
-		, startX: 200
-		, startY: 406
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 11
-		, edgeOffsetBottom: 88
-		, edgeOffsetLeft: 6
-		, edgeOffsetRight: 83
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	},
-	'pink-girl': {
-		sprite: 'images/char-pink-girl.png'
-		, startX: 200
-		, startY: 404
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 12
-		, edgeOffsetBottom: 88
-		, edgeOffsetLeft: 12
-		, edgeOffsetRight: 88
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	},
-	'princess-girl': {
-		sprite: 'images/char-princess-girl.png'
-		, startX: 200
-		, startY: 413
-		, minX: 0
-		, maxX: 400
-		, minY: 0
-		, maxY: 400
-		, edgeOffsetTop: 2
-		, edgeOffsetBottom: 88
-		, edgeOffsetLeft: 13
-		, edgeOffsetRight: 87
-		, offScreenLeft: -271
-		, offScreenRight: 600
-	}
-};
-
 var Player = function(character) {
-	this.sprite = playerImages[character].sprite;
-	this.startX = playerImages[character].startX;
-	this.startY = playerImages[character].startY;
-	this.minX = playerImages[character].minX;
-	this.maxX = playerImages[character].maxX;
-	this.minY = playerImages[character].minY;
-	this.maxY = playerImages[character].maxY;
-	this.edgeOffsetTop = playerImages[character].edgeOffsetTop;
-	this.edgeOffsetBottom = playerImages[character].edgeOffsetBottom;
-	this.edgeOffsetLeft = playerImages[character].edgeOffsetLeft;
-	this.edgeOffsetRight = playerImages[character].edgeOffsetRight;
-	this.offScreenLeft = playerImages[character].offScreenLeft;
-	this.offScreenRight = playerImages[character].offScreenRight;
+	this.sprite = appImages[character].sprite;
+	this.startX = appImages[character].startX;
+	this.startY = appImages[character].startY;
+	this.minX = appImages[character].minX;
+	this.maxX = appImages[character].maxX;
+	this.minY = appImages[character].minY;
+	this.maxY = appImages[character].maxY;
+	this.characterOffsetTop = appImages[character].characterOffsetTop;
+	this.characterOffsetBottom = appImages[character].characterOffsetBottom;
+	this.characterOffsetLeft = appImages[character].characterOffsetLeft;
+	this.characterOffsetRight = appImages[character].characterOffsetRight;
+	this.offScreenLeft = appImages[character].offScreenLeft;
+	this.offScreenRight = appImages[character].offScreenRight;
+	this.imageHeight = appImages[character].imageHeight;
+	this.imageWidth = appImages[character].imageWidth;
 	this.flyOffSpeed = 0;
+	this.drawAngle = 0;
 	this.resetStart();
 };
 
 Player.prototype = Object.create(Character.prototype);
-
-Player.prototype.flyOffScreen = function(dt, speed) {
-	while (this.x < this.offScreenRight) {
-		this.x += dt * speed;
-	}
-};
 
 Player.prototype.resetStart = function() {
 	this.x = this.startX;
@@ -208,14 +227,15 @@ Player.prototype.update = function(dt) {
 
 	if (this.flyOffSpeed > 0) {
 		if (this.x < this.offScreenRight) {
-			// Make hte player fly off screen to the upper right.
+			// Make the player fly off screen to the right.
 			this.x += dt * this.flyOffSpeed;
-			this.y --;
+			this.drawAngle += 10;
 		} else {
 			this.flyOffSpeed = 0;
+			this.drawAngle = 0;
 			// Reset all enemies.
 			for (i = 0; i < allEnemies.length; i++) {
-				allEnemies[i].currentSpeed = allEnemies[i].speed;
+				allEnemies[i].stopped = false;
 				allEnemies[i].getRandomStart();
 			}
 			this.resetStart();
@@ -223,23 +243,38 @@ Player.prototype.update = function(dt) {
 	} else {
 		// Iterate over the enemies and check for an image overlap on the player.
 		for (i = 0; i < allEnemies.length; i++) {
-			if (this.leftEdge() < allEnemies[i].rightEdge()
-			    && this.rightEdge() > allEnemies[i].leftEdge()
-			    && this.topEdge() < allEnemies[i].bottomEdge()
-			    && this.bottomEdge() > allEnemies[i].topEdge()) {
+			if (this.getCharacterLeftEdge() < allEnemies[i].getCharacterRightEdge()
+			    && this.getCharacterRightEdge() > allEnemies[i].getCharacterLeftEdge()
+			    && this.getCharacterTopEdge() < allEnemies[i].getCharacterBottomEdge()
+			    && this.getCharacterBottomEdge() > allEnemies[i].getCharacterTopEdge()) {
 				// We have a collision. Transfer the motion from the enemy to the player.
-				this.flyOffSpeed = allEnemies[i].currentSpeed;
-				allEnemies[i].currentSpeed = 0;
+				this.flyOffSpeed = allEnemies[i].speed;
+				allEnemies[i].stopped = true;
 			}
 		}
 	}
 
 };
 
-
-
 Player.prototype.render = function() {
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	if (this.drawAngle != 0) {
+		var
+			imgCenter = this.x + (this.imageWidth / 2)
+			, imgMiddle = this.y + (this.imageHeight / 2);
+		ctx.translate(imgCenter, imgMiddle);
+		ctx.rotate(this.drawAngle * Math.PI / 180);
+		ctx.drawImage(
+		    Resources.get(this.sprite)
+		    , -1 * this.imageWidth / 2
+		    , -1 * this.imageHeight / 2
+		    , this.imageWidth
+		    , this.imageHeight
+		);
+		ctx.rotate(-1 * this.drawAngle * Math.PI / 180);
+		ctx.translate(-1 * imgCenter, -1 * imgMiddle);
+	} else {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
 };
 
 Player.prototype.handleInput = function(key) {
@@ -271,12 +306,12 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [
-	new Enemy('boy', 300)
+	new Enemy('bug', 300)
 	, new Enemy('bug', 400)
 	, new Enemy('bug', 500)
 ];
 
-var player = new Player('pink-girl');
+var player = new Player('boy');
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
